1a2868e1d627eab2660ab4a9dfafee17d8a145b0
Jocelin Mendoza <jmendoza11@ggc.edu>
2026-04-09 17:10:57 -0400

    "created Room, RoomMap classes and did Room text files."

--- src/Room.java
+++ src/Room.java
@@ -5,18 +5,24 @@ public class Room {
     private String roomId;
     private String roomName;
     private String roomDescription;
-    private ArrayList<String> exits;
+    private String building;
+    private ArrayList<Room> exits;
     private ArrayList<Item> inventory;
-    //sum for keep track puzzle
-    //sum for keep track monster??
+    private Monster monster;
+    private Puzzle puzzle;
+    private boolean requiresValidFlashlight;
 
     //constructor
-    public Room(String roomId, String roomName, String roomDescription) {
+    public Room(String roomId, String roomName, String roomDescription, String building, boolean requiresValidFlashlight) {
         this.roomId = roomId;
         this.roomName = roomName;
         this.roomDescription = roomDescription;
+        this.building = building;
         this.exits = new ArrayList<>();
         this.inventory = new ArrayList<>();
+        this.monster = null;
+        this.puzzle = null;
+        this.requiresValidFlashlight = requiresValidFlashlight;
     }
 
     //getters
@@ -31,6 +37,34 @@ public class Room {
     public String getRoomDescription() {
         return roomDescription;
     }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public ArrayList<Room> getExits() {
+        return exits;
+    }
+
+    public ArrayList<Item> getInventory() {
+        return inventory;
+    }
+
+    public Monster getMonster() {
+        return monster;
+    }
+
+    public Puzzle getPuzzle() {
+        return puzzle;
+    }
+
+    public boolean requiresValidFlashlight() {
+        return requiresValidFlashlight;
+    }
+
+    //setters
+
+
 
     //methods
     public void addExit(String exit) {
