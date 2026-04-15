538d5e765cfcc7df2dcdc88d71ea7c7940d7180f
asliee pena <apenacabrera@ggc.edu>
2026-04-14 23:04:15 -0400

    Updated Player class per SRS

--- src/Player.java
+++ src/Player.java
@@ -78,42 +78,24 @@ public class Player extends Character {
 
     // Restart
     public void restartGame() {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Are you sure you want to restart the game? (yes/no): ");
-        String answer = input.nextLine();
-
-        if (answer.equalsIgnoreCase("yes")) {
-            if (map != null) {
-                map = new GameMap();
-            }
-
-            startGame();
-            System.out.println("The game has been restarted.");
+        vialCount = 0;
+        capacity = 5;
+        currentRoom = "R1";
+        equippedWeapon = null;
+        if (map != null) {
+            map.generateRooms();
+            map.loadPuzzles();
+            map.spawnMonsters();
+            map.loadItems();
         }
-        else {
-            System.out.println("Restart cancelled.");
-        }
+        System.out.println("Game has been restared.");
     }
 
     // Quit
     public void quitGame() {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Do you want to save the game before quitting? (yes/no): ");
-        String answer = input.nextLine();
-
-        if (answer.equalsIgnoreCase("yes")) {
-            if (map != null) {
-                map.saveGame(this);
-            }
-            System.out.println("Game saved. Exiting game.");
-        }
-        else {
-            System.out.println("Exiting game without saving.");
-        }
-
+        System.out.println("Exiting game.");
         System.exit(0);
+
     }
 
     // Escape Game
@@ -133,23 +115,5 @@ public class Player extends Character {
         }
     }
 
-    // Inventory
-    public void viewInventory() {
-        System.out.println("Inventory:");
 
-        // This is a temporary placeholder until the real Inventory structure is added
-        System.out.println("Weapons: ");
-        System.out.println("Items: ");
-        System.out.println("Quest Items: ");
-        System.out.println("Tools: ");
-
-        if (equippedWeapon != null) {
-            System.out.println("Equipped Weapon: " + equippedWeapon);
-        }
-        else {
-            System.out.println("Equipped Weapon: none");
-        }
-
-        System.out.println("Available space: " + capacity);
-    }
 }
