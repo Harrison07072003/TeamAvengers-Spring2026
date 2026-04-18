e756f1ccb2127683e944036f6de7800c3acfb255
Jose Munoz-Suastegui <jmunozsuastegui@ggc.edu>
2026-04-17 20:03:08 -0400

    yes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
-    String a = "Gethsemane";
-    String b = "Jose Munoz-suastegui";
-    String c = "Harrison";
-    String d = "Asliee";
-    String e = "Jocelin";
-
     public static void main(String[] args) {
-        GameController gc = new GameController();
-        gc.run();
+        GameController game = new GameController();
+        game.run();
     }
 }
