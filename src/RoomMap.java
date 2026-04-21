a4006d523af8bef6c52e6bb5acea2554a4327873
Harrison <hallen5@ggc.edu>
2026-04-20 21:56:08 -0400

    Added RoomMap

--- src/RoomMap.java
+++ src/RoomMap.java
@@ -1,3 +1,19 @@
-public class RoomMap {
+import java.util.ArrayList;
+public class RoomMap implements GameMap {
     //fields
+    ArrayList<Room> rooms;
+    //constructor
+    public RoomMap(){
+        this.rooms = new ArrayList<>();
+    }
+
+
+    //Takes in the current room number and is able to get the actual room data
+    public Room getRoom(String num) {
+        int roomNumber = Integer.parseInt(num.substring(1));
+        return rooms.get(roomNumber-1);
+    }
+    public ArrayList<Room> getRooms(){
+        return this.rooms;
+    }
 }
